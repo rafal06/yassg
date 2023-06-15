98b63b21ab5e72b9826b117e6f792030718db5b7
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2023-06-15 13:56:00 +0200

    Add unit tests for inserting components with zero and multiple parameters

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -16,7 +16,23 @@ fn inserting_variables() {
 }
 
 #[test]
-fn inserting_components() {
+fn inserting_components_with_no_parameters() {
+    let components = HashMap::from([
+        ("Greeting".to_string(), Component::new( "Greeting".to_string(), "<h1>Hello, World!</h1>".to_string()))
+    ]);
+    let file = HtmlFile::new(
+        "index".to_string(),
+        r#"<div><Greeting /></div>"#.to_string(),
+    );
+
+    let file_inserted = file.insert_components(&components);
+    let expected = r#"<div><h1>Hello, World!</h1></div>"#;
+
+    assert_eq!(file_inserted.content, expected);
+}
+
+#[test]
+fn inserting_components_with_one_parameter() {
     let components = HashMap::from([
         ("Greeting".to_string(), Component::new( "Greeting".to_string(), "<h1>Hello, {{name}}!</h1>".to_string()))
     ]);
@@ -27,6 +43,24 @@ fn inserting_components() {
 
     let file_inserted = file.insert_components(&components);
     let expected = r#"<div><h1>Hello, World!</h1></div>"#;
-    
+
+    assert_eq!(file_inserted.content, expected);
+}
+
+#[test]
+fn inserting_components_with_multiple_parameters() {
+    let components = HashMap::from([
+        ("Paragraph".to_string(), Component::new(
+            "Paragraph".to_string(),
+            "<h{{heading_level}}>{{title}}</h{{heading_level}}><p>{{text}}</p>".to_string()))
+    ]);
+    let file = HtmlFile::new(
+        "index".to_string(),
+        r#"<div><Paragraph heading_level="2" title="Lorem" text="Lorem ipsum dolor" /></div>"#.to_string(),
+    );
+
+    let file_inserted = file.insert_components(&components);
+    let expected = r#"<div><h2>Lorem</h2><p>Lorem ipsum dolor</p></div>"#;
+
     assert_eq!(file_inserted.content, expected);
 }
